d8d1faa4c7c8d0b6bffc91ecb3845dba7ff83a20
Nim Yuan <nim.yuan@additude.eu>
2020-04-22 23:09:55 +0200

    Add method doStuff()

--- src/App.java
+++ src/App.java
@@ -2,4 +2,7 @@ public class App {
     public static void main(String[] args) {
         System.out.println("App3");
     }
+    public void doStuff(){
+        System.out.println("Doing Stuff");
+    }
 }
